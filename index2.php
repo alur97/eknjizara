619a43af4524294696eb995012491bd1ca160d8a
vvasiljevic <58689828+vvasiljevic@users.noreply.github.com>
2020-01-15 22:56:06 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -9,7 +9,7 @@ require "korisnik/template/header.php";
         <div class="row_header">
           <h1>KNJIŽARA MS</h1>
           <br>
-        </div>
+        </div> 
           				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
 
                         <div class="post">
